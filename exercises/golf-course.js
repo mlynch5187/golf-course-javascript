60c47845ed913277ddbb4efb4f50e026dfc09fe2
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-08 07:31:42 -0700

    Complete all exercises and pass all tests

--- exercises/golf-course.js
+++ exercises/golf-course.js
@@ -8,9 +8,14 @@ class GolfCourse  {
   }
 
   checkInGroup(group) {
-    for (var i = 0; i < group.length; i++) {
+    if (group.length <= this.openings) {
+      for (var i = 0; i < group.length; i++) {
         this.currentlyPlaying.unshift(group[i].name);
         this.openings--;
+      }
+      return `You're checked in. Have fun!`
+    } else {
+      return `Sorry, we are currently booked! Please come back later.`
     }
   }
 }
